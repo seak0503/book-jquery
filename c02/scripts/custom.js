5d1e5c7143c683b2eb1f4a6d81ec44d4baff2b4c
Shota Yamahira <seak0503@gmail.com>
2016-05-06 07:21:28 +0900

    02-02

--- c02/scripts/custom.js
+++ c02/scripts/custom.js
@@ -4,3 +4,21 @@ $(function() {
     $('.wrapper-headerinfo').slideToggle(400);
   });
 });
+
+$(function() {
+  $(window)
+  .on('resize', function() {
+    var $nav = $('.nav-global');
+    var listFloat = $nav.find('li').css('float');
+    if (listFloat == 'none') {
+      $nav.css('display', 'none');
+    } else {
+      $nav.css('display', 'block');
+    }
+  });
+
+  $('.menubtn > a')
+  .on('click', function() {
+    $('.nav-global').slideToggle(400);
+  });
+});
