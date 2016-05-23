1543af717a52e265bf3a7d3a71b7af735f8226c6
Shota Yamahira <seak0503@gmail.com>
2016-05-24 07:35:02 +0900

    p197まで完了

--- c03/scripts/custom.js
+++ c03/scripts/custom.js
@@ -126,3 +126,63 @@ $(function() {
     }
   });
 });
+
+/**
+ * 03-06 スライドショー
+ */
+$(function() {
+  var intervalId;
+  setTimer();
+
+  function setTimer() {
+    intervalId = setInterval(autoClick, 5000);
+  }
+
+  function autoClick() {
+    $('.slide').children('a.next').click();
+  }
+
+  function changeImage($click) {
+    //console.log($click);
+    var $current = $click.siblings('.container').children('.current');
+    var $new;
+    var findSelector = '';
+
+    if ($click.hasClass('next')) {
+      $new = $current.next();
+      findSelector = ':first-child';
+    } else {
+      $new = $current.prev();
+      findSelector = ':lst-child';
+    }
+
+    if ($new.length == 0) {
+      $new = $current.siblings(findSelector);
+    }
+    $current.removeClass('current');
+    $new.addClass('current');
+    setTimer();
+  }
+
+  $('.slide')
+  .on('click', '> a', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    clearInterval(intervalId);
+    changeImage($(this));
+  });
+});
+
+/**
+ * 03-07 Ajaxの基礎
+ */
+$(function() {
+  $('#fetch').on('click', function(event) {
+    event.preventDefault();
+    $this = $(this);
+    var ajaxUrl = $this.attr('href');
+    $.get(ajaxUrl, function(data) {
+      console.dir(data);
+    });
+  });
+});
