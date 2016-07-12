fd2a03705cf983e2323ff11d3c8518df35c74599
shyamahira <shyamahira@bbtower.co.jp>
2016-07-13 06:59:11 +0900

    04-03 完了

--- c04/scripts/custom.js
+++ c04/scripts/custom.js
@@ -38,8 +38,18 @@ $(function() {
 
 /**
  * 04-02　ウィンドウ上端でグローバルナビゲーションを固定する
+ * 04-03　指定した位置でサイドメニューを固定する
  */
 $(function () {
+
+	function mediaDetect(query) {
+		if (window.matchMedia) {
+			return window.matchMedia(query).matches;
+		} else {
+			return false;
+		}
+	}
+
 	$(window).on('scroll', function () {
 		var scrollValue = $(this).scrollTop();
 		$('.fixedmenu')
@@ -55,30 +65,39 @@ $(function () {
 
 		var $this = $(this);
 
-		if ($this.data('initial') <= object.posY) {
-			//要素を固定
-			if (!$this.hasClass('fixed')) {
-				var $substitute = $('<div></div>');
-				$substitute
-				.css({
-					'margin':'0',
-					'padding':'0',
-					'font-size':'0',
-					'height':'0'
-				})
-				.addClass('substitute')
-				.height($this.outerHeight(true))
-				.width($this.outerWidth(true));
-
-				$this
-				.after($substitute)
-				.addClass('fixed')
-				.css({top: 0});
-			}
+		if ($this.hasClass('noresponsive') && mediaDetect('(max-width:600px)')) {
+			//なにもしない
 		} else {
-			//要素の固定を解除
-			$this.next('.substitute').remove();
-			$this.removeClass('fixed');
+			var offsetTop = 0;
+			if ($this.data('offsettop')) {
+				offsetTop = $this.data('offsettop');
+			}
+
+			if ($this.data('initial') - offsetTop <= object.posY) {
+				//要素を固定
+				if (!$this.hasClass('fixed')) {
+					var $substitute = $('<div></div>');
+					$substitute
+					.css({
+						'margin':'0',
+						'padding':'0',
+						'font-size':'0',
+						'height':'0'
+					})
+					.addClass('substitute')
+					.height($this.outerHeight(true))
+					.width($this.outerWidth(true));
+
+					$this
+					.after($substitute)
+					.addClass('fixed')
+					.css({top: offsetTop});
+				}
+			} else {
+				//要素の固定を解除
+				$this.next('.substitute').remove();
+				$this.removeClass('fixed');
+			}
 		}
 	});
 });
