70b5906be5af7fca25ebab33da34a57ee11f4116
shyamahira <shyamahira@bbtower.co.jp>
2016-08-16 20:15:05 +0900

    03-02完了

--- c03/scripts/custom.js
+++ c03/scripts/custom.js
@@ -16,3 +16,35 @@ $(function(){
     .slideUp('fast');
   });
 });
+
+/**
+ * 03-02　ドロップダウンメニュー
+ */
+$(function () {
+    $('.dropdown-menu').children('a')
+    .on('click', function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        var $this = $(this);
+
+        //複数のドロップダウンメニューがあるときの対策
+        $this.parent().siblings('li').children('a')
+        .removeClass('open')
+        .next().hide();
+
+        if ($this.hasClass('open')) {
+            $this.removeClass('open')
+            .next().hide();
+            $('html').off('click', closeItems);
+        } else {
+            $this.addClass('open')
+            .next().show();
+            $('html').on('click', closeItems);
+        }
+
+        function closeItems() {
+            $this.removeClass('open')
+            .next().hide();
+        }
+    });
+});
