d6ed8642e661b24db6dded2338ae593012909bfc
shyamahira <shyamahira@bbtower.co.jp>
2016-08-30 22:00:27 +0900

    04-02 step2まで完了

--- c04/scripts/custom.js
+++ c04/scripts/custom.js
@@ -6,25 +6,52 @@ $(function () {
   function showModal(event) {
     event.preventDefault();
 
+    var $shade = $('<div></div>');
+    $shade
+    .attr('id', 'shade')
+    .on('click', hideModal);
+
     var $modalWin = $('#modalwin');
     var $window = $(window);
     var posX = ($window.width() - $modalWin.outerWidth()) / 2;
     var posY = ($window.height() - $modalWin.outerHeight()) / 2;
 
     $modalWin
+    .before($shade)
     .css({left: posX, top: posY})
     .removeClass('hide')
     .addClass('show')
-    .on('click', 'button', function () {
+    .on('click', 'button, .modal-close', function () {
       hideModal();
     });
   }
   //モーダルウィンドウを閉じる
   function hideModal() {
+    $('#shade').remove();
     $('#modalwin')
     .removeClass('show')
     .addClass('hide');
   }
 
   $('.show-modal').on('click', showModal);
+});
+
+/**
+ * 04-02　ウィンドウ上端でグローバルナビゲーションを固定する
+ */
+$(function () {
+  $(window).on('scroll', function () {
+    var scrollValue = $(this).scrollTop();
+    //console.log(scrollValue);
+    $('.fixedmenu')
+    .trigger('customScroll', {posY: scrollValue});
+
+    $('.fixedmenu')
+    .each(function(){
+      console.log(this);
+    })
+    .on('customScroll', function (event,  object) {
+      console.log('customScroll %s', object.posY)
+    });
+  });
 });
