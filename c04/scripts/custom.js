00d32295ea5bd87ceca30fb13c3c9ba0272362ac
shyamahira <shyamahira@bbtower.co.jp>
2016-08-31 07:04:12 +0900

    04-02完了

--- c04/scripts/custom.js
+++ c04/scripts/custom.js
@@ -42,16 +42,46 @@ $(function () {
 $(function () {
   $(window).on('scroll', function () {
     var scrollValue = $(this).scrollTop();
-    //console.log(scrollValue);
+    console.log(scrollValue);
     $('.fixedmenu')
     .trigger('customScroll', {posY: scrollValue});
+  });
 
-    $('.fixedmenu')
-    .each(function(){
-      console.log(this);
-    })
-    .on('customScroll', function (event,  object) {
-      console.log('customScroll %s', object.posY)
-    });
+  $('.fixedmenu')
+  .each(function(){
+    var $this = $(this);
+    $this.data('initial', $this.offset().top);
+    console.log('offset %s', $this.offset().top);
+  })
+  .on('customScroll', function (event,  object) {
+    console.log('customScroll %s', object.posY)
+
+    var $this = $(this);
+
+    if ($this.data('initial') <= object.posY) {
+      //要素を固定
+      if(!$this.hasClass('fixed')) {
+        var $substitute = $('<div></div>');
+        $substitute
+        .css({
+          'margin':'0',
+          'padding':'0',
+          'font-size':'0',
+          'height':'0'
+        })
+        .addClass('substitute')
+        .height($this.outerHeight(true))
+        .width($this.outerWidth(true));
+
+        $this
+        .after($substitute)
+        .addClass('fixed')
+        .css({top: 0});
+      }
+    } else {
+      //要素の固定を解除
+      $this.next('.substitute').remove();
+      $this.removeClass('fixed');
+    }
   });
 });
