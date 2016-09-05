38bd73c88384621b61106211d62b2b44fa71b05b
Shota Yamahira <seak0503@gmail.com>
2016-09-06 07:29:27 +0900

    06-01 step1完了

---
+++ c06/scripts/custom.js
@@ -0,0 +1,16 @@
+$(function () {
+  /**
+   * 06-01 カルーセル
+   */
+  $('#carousel')
+  .find('.thumbs-list').each(function () {
+    var $this = $(this);
+
+    //#carouselの幅を設定
+    var listWidth = $this.outerWidth() * 2;
+    $this.parent().width(listWidth);
+
+    //thumbs-listをクローン
+    $this.clone().insertAfter($this);
+  });
+});
