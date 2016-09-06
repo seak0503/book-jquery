dc9fa7d6b03cf5067d8fddfb7f322df8e79e9b0b
Shota Yamahira <seak0503@gmail.com>
2016-09-07 07:04:59 +0900

    06-01 step2完了

--- c06/scripts/custom.js
+++ c06/scripts/custom.js
@@ -12,5 +12,30 @@ $(function () {
 
     //thumbs-listをクローン
     $this.clone().insertAfter($this);
+  })
+  .end()
+  .on('click', 'a.prev', function(event){
+    event.preventDefault();
+    doCarousel($(this), 1);
+  })
+  .on('click', 'a.next', function(event){
+    event.preventDefault();
+    doCarousel($(this), -1);
   });
+
+  /* カルーセルアニメーションを実行する関数 */
+  function doCarousel(btn, direction) {
+    var $btn = btn;
+    var $carousel = $btn.closest('.carousel');
+    var $imgContainer = $carousel.find('.thumbs');
+
+    //移動量を計算
+    var move = direction * $carousel.width() + $imgContainer.position().left;
+
+    $imgContainer.filter(':not(:animated)')
+    .animate({left: move},
+      {
+        duration:800
+      });
+   }
 });
