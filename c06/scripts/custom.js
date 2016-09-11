d99a95688841418e426f49b5522f6670b3659f66
shyamahira <shyamahira@bbtower.co.jp>
2016-09-12 07:22:40 +0900

    step06-02 完了

--- c06/scripts/custom.js
+++ c06/scripts/custom.js
@@ -31,8 +31,6 @@ $(function () {
 
     //移動量を計算
     var move = direction * $carousel.width() + $imgContainer.position().left;
-    console.log("moveデバッグ");
-    console.log(move);
 
     $imgContainer.filter(':not(:animated)')
     .animate({left: move},
@@ -47,11 +45,69 @@ $(function () {
           //[<]がクリックされたとき
           if (direction == 1 && 0 <= $this.position().left) {
             resetPos = $this.position().left - $imgList.outerWidth();
-            console.log("resetPosデバッグ");
-            console.log(resetPos);
+            $this.css({"left": resetPos});
+          }
+          //[>]がクリックされたとき
+          if (direction == -1 && $imgList.outerWidth() <= Math.abs($this.position().left)) {
+            resetPos = $this.position().left + $imgList.outerWidth();
             $this.css({"left": resetPos});
           }
         }
       });
    }
+});
+
+/**
+ * 06-02　大きな画像を表示する
+ */
+$(function () {
+  $('.thumbs-list')
+  .on('click', 'li', function () {
+    var $this = $(this);
+    var myIndex = $this.prevAll().length;
+    /* サムネイルのハイライト */
+    $this
+    .closest('.thumbs').find('li')
+    .removeClass('selected')
+    .end()
+    .children('.thumbs-list').each(function () {
+      $(this).children('li').eq(myIndex).addClass('selected');
+    });
+    var $img = $this.find('img');
+    changeImage($img.data('imgpath'));
+    changeCaption($this.data('caption'));
+  });
+
+  /* キャプションを表示する */
+  function changeCaption(caption) {
+    $('.gallery').find('.caption')
+    .stop()
+    .animate({'text-indent':-50, 'opacity':0},
+    {
+      duration: 400,
+      complete: function () {
+        $(this)
+        .text(caption)
+        .css({'text-indent':50})
+      }
+    })
+    .animate({'text-indent':0, 'opacity':1});
+  }
+
+  /* 画像を切り替える */
+  function changeImage(path) {
+    var $image = $('<img>').attr('src', path).css('opacity', 0);
+    $('.mainimage').append($image);
+    $image.siblings('img').stop();
+    $image.animate({opacity: 1},
+    {
+      duration: 1000,
+      complete: function () {
+        $(this)
+        .addClass('front')
+        .siblings('img')
+        .remove();
+      }
+    });
+  }
 });
